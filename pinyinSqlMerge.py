56c698daad836f80f0c4bf79af8f1e6081f1e6e5
世纪 <meishijiemeimei@163.com>
2018-03-02 15:24:48 +0800

    1
    1

--- pinyinSqlMerge.py
+++ pinyinSqlMerge.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from pypinyin import pinyin, lazy_pinyin, Style
 import sqlite3
-app = Flask(__name__)
 
 # 兼容python2.7
 import sys
@@ -17,9 +16,7 @@ def search(temp):
     # print search_pinyin
     conn = sqlite3.connect('e.db')
     c = conn.cursor()
-    i = 1
-    num = i*20
-    cursor = c.execute("SELECT * from lyc limit "+str(num)+","+str(num+20))
+    cursor = c.execute("SELECT * from lyc")
     for row in cursor:
         content = row[1]
         lyc_name = row[6]
@@ -29,17 +26,18 @@ def search(temp):
             pinyin_temp = pinyin(item,style=Style.FINALS)[-2:]
             if(search_pinyin == pinyin_temp):
                 # print (u'歌曲:'+lyc_name)
-                # print (item)
+                print (item)
                 # print ('-----')
                 all.append(item)
     conn.close()
 
-@app.route('/')
+# @app.route('/')
 def index():
     st = '意义'
     search(st);
-    return "搜索 %s <br /> %s " % (st,'<br/>'.join(all))
+    # return "搜索 %s <br /> %s " % (st,'<br/>'.join(all))
+    print "搜索 %s <br /> %s " % (st,'<br/>'.join(all))
     
-if __name__ == '__main__':
-    app.run()
-    
+index()
+# if __name__ == '__main__':
+    # app.run()
